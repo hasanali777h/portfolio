ffd6b31d36edbee1ac9dd42f5869caad80332807
hasanali777h <hasanali777h@gmail.com>
2025-11-02 05:25:36 +0500

    some more fixes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
-// const basePath = process.env.NODE_ENV === "production" ? "/Resume-Nextjs" : "";
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
-
+const basePath = process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_BASE_PATH : "";
 
 const nextConfig: NextConfig = {
   output: "export",
@@ -10,6 +8,12 @@ const nextConfig: NextConfig = {
   assetPrefix: basePath,
   images: {
     unoptimized: true,
+    localPatterns: [
+      {
+        pathname: '/public/images/**',
+        search: '',
+      }
+    ]
   },
   trailingSlash: true,
   eslint: {
